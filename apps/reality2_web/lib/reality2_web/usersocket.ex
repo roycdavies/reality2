47a93a91a644814b279c701b770e9c85ed58b337
Roy Davies <roy.c.davies@ieee.org>
2024-03-21 08:36:12 +1300

    WIP subscriptions elixir
    https://blog.appsignal.com/2023/08/08/a-deep-dive-into-subscriptions-with-absinthe.html

--- apps/reality2_web/lib/reality2_web/usersocket.ex
+++ apps/reality2_web/lib/reality2_web/usersocket.ex
@@ -2,10 +2,22 @@ defmodule Reality2Web.UserSocket do
   use Phoenix.Socket
   use Absinthe.Phoenix.Socket, schema: Reality2Web.Schema
 
-  def connect(params, socket) do
-    IO.puts("UserSocket.connect params: #{inspect(params)}")
+  @impl true
+  def connect(query_params, socket, connect_info) do
+    IO.puts("Connect #{inspect(connect_info)}")
+    case authorize_user(socket, query_params) do
+      {:ok, user} ->
+        {:ok, Absinthe.Phoenix.Socket.put_options(socket, context: %{current_user: user})}
+      {:error, _reason} ->
+        # Reject the connection
+        :error
+    end
+  end
 
-    {:ok, socket}
+  defp authorize_user(socket, query_params) do
+    # Fetch the user here (for example, from a token in the query_params)
+    IO.puts("Checking User, #{inspect(socket)} #{inspect(query_params)}")
+    {:ok, :fred}
   end
 
   def id(socket) do
